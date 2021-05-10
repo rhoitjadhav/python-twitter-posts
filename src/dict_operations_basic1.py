b1c7cfde8cc170ed7023e85334bd5fa5d656810d
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-10 12:01:54 +0530

    Dict Operations Basic 1

---
+++ src/dict_operations_basic1.py
@@ -0,0 +1,29 @@
+'''
+Python Dictionary (Basic 1)
+    - It's an unordered collection of items
+    - Mutable Datatype
+    - Key/Value pair data structure
+'''
+
+# Create dictionary of student
+
+student = {
+    'name': 'Alex Hales',
+    'age': 25,
+    'dob': '10/04/1996'
+}
+
+# Access attributes of student
+print('Name:', student['name'])
+print('Age:', student['age'])
+print('DOB:', student['dob'])
+
+
+'''
+Output:
+
+Name: Alex Hales
+Age: 25
+DOB: 10/04/1996
+'''
+
