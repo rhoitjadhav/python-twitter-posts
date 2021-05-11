23ec6cbfdc6b522c10324b36e42d34e04b33ef00
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-11 11:16:19 +0530

    Dict Operations Basic 2

---
+++ src/dict_operations_basic2.py
@@ -0,0 +1,32 @@
+'''
+Python Dictionary (Basic 2)
+    - It's an unordered collection of items
+    - Mutable Datatype
+    - Key/Value pair data structure
+'''
+
+# Create dictionary of student
+student = {
+    'name': 'Alex Hales',
+    'age': 25,
+    'dob': '10/04/1996'
+}
+
+# Add element in dictionary
+student['address'] = 'London'
+
+# Update element in dictionary
+student['dob'] = '11/04/1996'
+
+# Display dictionary
+print(student)
+
+'''
+Output:
+{
+    'name': 'Alex Hales', 
+    'age': 25, 
+    'dob': '11/04/1996', 
+    'address': 'London'
+}
+'''
