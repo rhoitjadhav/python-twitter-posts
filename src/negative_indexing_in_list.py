5beedd863f28276ef512d6e9e11a4a8dde027449
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-21 10:31:36 +0530

    Negative Indexing in List source

---
+++ src/negative_indexing_in_list.py
@@ -0,0 +1,20 @@
+'''
+Python Negative Indexing in List
+
+Example: Create numbers list and print 
+         last 3 numbers
+'''
+# Create numbers list
+numbers = [1, 2, 3, 4, 5]
+
+# Print last 3 numbers
+print(numbers[-1])
+print(numbers[-2])
+print(numbers[-3])
+
+'''
+Output:
+5
+4
+3
+'''
