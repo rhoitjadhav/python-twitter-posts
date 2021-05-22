e7b36baecd3c09124674d0e4bfb5a17cdec20afd
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-22 11:49:23 +0530

    Tuple Operations Basic 1 source

---
+++ src/tuple_operations_basic1.py
@@ -0,0 +1,20 @@
+'''
+Python Tuple Operations (Basic 1)
+
+Example: Create and print tuple
+'''
+# Create numbers tuple
+numbers = (1, 2, 3, 4, 5)
+
+# Print tuple
+for num in numbers:
+    print(num)
+
+'''
+Output:
+1
+2
+3
+4
+5
+'''
