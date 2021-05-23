615c249f9d3ec24d737d47ec7357e3a8bfdfc19f
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-23 12:32:45 +0530

    Tuple Operations Basic 2 source

---
+++ src/tuple_operations_basic2.py
@@ -0,0 +1,18 @@
+'''
+Python Tuple Operations (Basic 2)
+
+Example: Update elements in tuple
+'''
+# Create numbers tuple
+numbers = (1, 3, 3, 4, 5)
+
+# Update element
+numbers[1] = 2
+
+'''
+Output:
+Traceback (most recent call last):
+  File "test/test.py", line 10, in <module>
+    numbers[1] = 2
+TypeError: 'tuple' object does not support item assignment
+'''
