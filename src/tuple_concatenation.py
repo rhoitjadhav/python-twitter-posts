9b92679904a0fcbf78c0b400851d7f8abbba922a
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-24 10:06:42 +0530

    Tuple Concatenation source

---
+++ src/tuple_concatenation.py
@@ -0,0 +1,18 @@
+'''
+Python Tuples Concatenation
+
+Example: Concatenate two tuples
+'''
+
+# Create numbers tuple
+player = ('Ja', 'Morant')
+points = (26, 4, 4)
+
+scores = player + points
+
+print(scores)
+
+'''
+Output:
+('Ja', 'Morant', 26, 4, 4)
+'''
