e60a293d896b220ba9581f4c609f7f66d33ae10b
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-26 09:46:22 +0530

    Negative Indexing in Tuple source

---
+++ src/negative_indexing_in_tuple.py
@@ -0,0 +1,21 @@
+'''
+Python Negative Indexing in Tuple
+
+Example: Create numbers tuple and print 
+         last 3 numbers
+'''
+
+# Create numbers tuple
+numbers = (1, 2, 3, 4, 5)
+
+# Print last 3 numbers
+print(numbers[-1])
+print(numbers[-2])
+print(numbers[-3])
+
+'''
+Output:
+5
+4
+3
+'''
