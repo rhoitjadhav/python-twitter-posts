c4e5fa338c0a267050485b378f89e3bf9795c4e9
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-27 11:54:03 +0530

    Tuple Packing and Unpacking source

---
+++ src/tuple_packing_and_unpacking.py
@@ -0,0 +1,27 @@
+'''
+Python Tuple Packing & Unpacking
+
+Example: Packing a tuple, unpacking into 
+         variables and print variables
+'''
+
+# Packing tuple
+stats = ("Bradley Beal", 33, "Wizards")
+
+
+# Unpacking tuple values
+name, points, team = stats
+
+
+# Print variables
+print("Name:", name)
+print("Points:", points)
+print("Team:", team)
+
+
+'''
+Output:
+Name: Bradley Beal
+Points: 33
+Team: Wizards
+'''
