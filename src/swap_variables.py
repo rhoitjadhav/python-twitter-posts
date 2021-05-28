5e698246c14d153fd4f4596a3ca7a06a46ed5eaa
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-28 10:50:30 +0530

    Swap Variables source

---
+++ src/swap_variables.py
@@ -0,0 +1,19 @@
+'''
+Python Swap Variables
+
+Example: Use tuple unpacking to swap variables
+'''
+a = 10
+b = 12
+
+a, b = b, a
+
+print("a:", a)
+print("b:", b)
+
+
+'''
+Output:
+a: 12
+b: 10
+'''
