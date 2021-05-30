19433394b0e2a1c311a36d50382d25e216ef587b
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-30 09:57:26 +0530

    Set Operations Basic 1 source

---
+++ src/set_operations_basic1.py
@@ -0,0 +1,23 @@
+'''
+Python Set (Basic 1)
+
+Example: Create and display set
+'''
+
+# Create numbers set
+numbers = {1, 2, 3, 4, 5}
+
+
+# Display
+for num in numbers:
+    print(num)
+
+
+'''
+Output:
+1
+2
+3
+4
+5
+'''
