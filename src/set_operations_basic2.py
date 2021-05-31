87150f1782ec493162ee77b639831269e0c199d1
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-31 09:29:56 +0530

    Set Operations Basic 2 source

---
+++ src/set_operations_basic2.py
@@ -0,0 +1,24 @@
+'''
+Python Set (Basic 2)
+
+Example: Add elements in set
+'''
+
+# Create numbers set
+numbers = {1, 2, 3}
+
+
+# Add numbers
+numbers.add(4)
+numbers.add(5)
+
+
+# Display
+print(numbers)
+
+
+'''
+Output:
+{1, 2, 3, 4, 5}
+'''
+
