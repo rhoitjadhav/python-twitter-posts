a03e2cd24448e4b5a13b8ce48abfc8194a592e2d
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-01 10:38:21 +0530

    Set Operations Basic 3 source

---
+++ src/set_operations_basic3.py
@@ -0,0 +1,22 @@
+'''
+Python Set (Basic 3)
+
+Example: Remove elements in set
+'''
+
+# Create numbers set
+numbers = {1, 2, 3, 4, 5}
+
+
+# Remove numbers 
+numbers.discard(6)
+numbers.remove(5)
+
+# Display
+print(numbers) 
+
+
+'''
+Output:
+{1, 2, 3, 4}
+'''
