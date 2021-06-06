491d5517ca4038118c7540e96b714e64e98b6e16
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-06 13:03:46 +0530

    Set Comprehensions source

---
+++ src/set_comprehensions.py
@@ -0,0 +1,16 @@
+'''
+Python Set Comprehensions
+
+Example: Create numbers set using comprehensions
+'''
+
+numbers = {i for i in range(5)}
+
+# Display
+print(numbers)
+
+'''
+Output:
+{0, 1, 2, 3, 4}
+'''
+
