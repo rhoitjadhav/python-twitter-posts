b0d830a6ff385594a950c24014a572bed703452c
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-08 10:09:56 +0530

    Set with Multiple Datatype source

---
+++ src/set_with_multiple_datatype.py
@@ -0,0 +1,20 @@
+'''
+Python Set with Multiple Datatypes
+
+Example: Store and print int, str, tuple 
+        elements in set
+'''
+
+# Create items
+items = {1, "2", (3, 4)}
+
+
+# Display
+print(items)
+
+
+'''
+Output:
+{1, '2', (3, 4)}
+'''
+
