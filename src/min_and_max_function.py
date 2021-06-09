5eaff64ff6ede0ef0d2bdf1b595299d506ebda22
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-09 09:17:45 +0530

    Min and Max function source

---
+++ src/min_and_max_function.py
@@ -0,0 +1,21 @@
+'''
+Python Min & Max function
+
+Example: Calculate min and max element of list
+'''
+
+# Create numbers list
+numbers = [1, 2, 3, 4, 5]
+
+
+# Display
+print("Min:", min(numbers))
+print("Max:", max(numbers))
+
+
+'''
+Output:
+Min: 1
+Max: 5
+'''
+
