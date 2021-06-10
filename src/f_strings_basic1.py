a6b0848cd4d50cf75325d62ef9dc780247862daa
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-10 09:29:53 +0530

    F-Strings Basic 1 source

---
+++ src/f_strings_basic1.py
@@ -0,0 +1,15 @@
+'''
+Python f-strings (Basic 1)
+
+Example: Create and display f-strings
+'''
+name = "Rohit"
+
+# Create & Display
+print(f"My name is {name}")
+
+'''
+Output:
+My name is Rohit
+'''
+
