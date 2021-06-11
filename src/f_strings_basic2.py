a2cf2d10044c8f1a2e549c5c474db004d8f4e63b
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-11 09:36:50 +0530

    F-Strings Basic 2 source

---
+++ src/f_strings_basic2.py
@@ -0,0 +1,19 @@
+'''
+Python f-strings (Basic 2)
+
+Example: f-string format floats
+'''
+pi = 3.141592653589793
+
+
+# Display
+print(f"Value of PI is {pi:.2f}")
+print(f"Value of PI is {pi:.3f}")
+
+
+'''
+Output:
+Value of PI is 3.14
+Value of PI is 3.142
+'''
+
