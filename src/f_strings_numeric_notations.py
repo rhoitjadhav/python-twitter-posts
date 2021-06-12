a506f1eab540eb0c872eb4c70686bd899af28d0e
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-12 09:49:25 +0530

    F-Strings Numeric Notations source

---
+++ src/f_strings_numeric_notations.py
@@ -0,0 +1,22 @@
+'''
+Python f-strings numeric notations
+
+Example: Print numberic notations of a number 
+        such as binary, octal, hexadecimal
+'''
+n = 10
+
+
+# Display
+print(f"Binary: {n:b}")
+print(f"Octal: {n:o}")
+print(f"Hexadecimal: {n:x}")
+
+
+'''
+Output:
+Binary: 1010
+Octal: 12
+Hexadecimal: a
+'''
+
