ecbcd50c5768db081d92046e245261b11a3e7903
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-15 10:31:33 +0530

    Lambda Function Square source

---
+++ src/lambda_function_square.py
@@ -0,0 +1,18 @@
+'''
+Python Lambda Function: Squaring
+
+Example: Create a square operation expression
+'''
+
+square = lambda n: n * n
+
+
+# Display
+print(square(10))
+
+
+'''
+Output:
+100
+'''
+
