e69439e44c312f39c75f93576ca6dba30626feda
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-16 09:42:37 +0530

    Map Function Basic 1 source

---
+++ src/map_function_basic1.py
@@ -0,0 +1,19 @@
+'''
+Python Map Function (Basic 1)
+
+Example: Convert strings of number into integer 
+'''
+
+numbers = ["1", "2", "3", "4"]
+
+result = map(int, numbers)
+
+# Display
+print(list(result))
+
+
+'''
+Output:
+[1, 2, 3, 4]
+'''
+
