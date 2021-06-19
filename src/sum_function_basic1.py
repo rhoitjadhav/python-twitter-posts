9738d3917145b98ae92c6063c73882be42e5d369
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-19 10:00:09 +0530

    Sum Function Basic 1 source

---
+++ src/sum_function_basic1.py
@@ -0,0 +1,21 @@
+'''
+Python Sum Function (Basic 1)
+
+Example: Sum of numbers 
+'''
+# Create numbers
+numbers_list = [1, 2, 3, 4, 5]
+numbers_tuple = (1, 2, 3)
+
+
+# Display
+print("List Sum:", sum(numbers_list))
+print("Tuple Sum:", sum(numbers_tuple))
+
+
+'''
+Output:
+List Sum: 15
+Tuple Sum: 6
+'''
+
