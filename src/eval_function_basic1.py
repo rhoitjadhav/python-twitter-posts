4f4c830a43491a25084cfd0d4707b50deac26939
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-20 08:11:50 +0530

    Eval Function Basic 1 source

---
+++ src/eval_function_basic1.py
@@ -0,0 +1,22 @@
+'''
+Python Eval Function (Basic 1)
+
+Example: Eval function examples 
+'''
+n = 3
+
+square = eval("n ** 2")
+cube = eval("n ** 3")
+
+
+# Display
+print("Square:", square)
+print("Cube:", cube)
+
+
+'''
+Output:
+Square: 9
+Cube: 27
+'''
+
