45b4b9f4437759df01d703caa4cbbd349d74caf2
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-22 09:42:38 +0530

    All Function Basic 1 source

---
+++ src/all_function_basic1.py
@@ -0,0 +1,24 @@
+'''
+Python All Function (Basic 1)
+
+Example: All function examples 
+'''
+iterable1 = [True, True, True]
+iterable2 = [True, True, False]
+
+
+result1 = all(iterable1)
+result2 = all(iterable2)
+
+
+# Display
+print("Result 1:", result1)
+print("Result 2:", result2)
+
+
+'''
+Output:
+Result 1: True
+Result 2: False
+'''
+
