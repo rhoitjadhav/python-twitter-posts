7174bbadf63288a82a577921ef4b6e78e4eb66ee
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-24 09:16:28 +0530

    Any Function Basic 1 source

---
+++ src/any_function_basic1.py
@@ -0,0 +1,24 @@
+'''
+Python Any Function (Basic 1)
+
+Example: Any function examples 
+'''
+iterable1 = [True, False, False]
+iterable2 = [False, False, False]
+
+
+result1 = any(iterable1)
+result2 = any(iterable2)
+
+
+# Display
+print("Result 1:", result1)
+print("Result 2:", result2)
+
+
+'''
+Output:
+Result 1: True
+Result 2: False
+'''
+
