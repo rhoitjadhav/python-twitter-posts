f006c75f6bf85d8c2a27f85c91fe84d91aefe1fc
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-25 08:42:43 +0530

    Any Function Basic 2 source

---
+++ src/any_function_basic2.py
@@ -0,0 +1,24 @@
+'''
+Python Any Function (Basic 2)
+
+Example: Any function examples 
+'''
+iterable1 = [0]
+iterable2 = {"0"}
+
+
+result1 = any(iterable1)
+result2 = any(iterable2)
+
+
+# Display
+print("Result 1:", result1)
+print("Result 2:", result2)
+
+
+'''
+Output:
+Result 1: False
+Result 2: True
+'''
+
