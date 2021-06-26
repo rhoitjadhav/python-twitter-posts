a62b88cea56488e6838e8c511c38edbfb3f3c60e
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-26 11:07:55 +0530

    Abs Function Basic 1 source

---
+++ src/abs_function_basic1.py
@@ -0,0 +1,19 @@
+'''
+Python Abs Function (Basic 1)
+
+Example: Abs function examples 
+'''
+
+# Display
+print(abs(-10))
+print(abs(-20.11))
+print(abs(2 - 3j))
+
+
+'''
+Output:
+10
+20.11
+3.605551275463989
+'''
+
