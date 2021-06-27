84a95f08a7aa7ab80dbaca205f58bcc2a25d9ee1
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-27 08:02:06 +0530

    Bin Function Basic 1 source

---
+++ src/bin_function_basic1.py
@@ -0,0 +1,20 @@
+'''
+Python Bin Function (Basic 1)
+
+Example: Bin function examples 
+'''
+
+num1 = 5
+num2 = 12
+
+# Display
+print("Binary:", bin(num1))
+print("Binary:", bin(num2))
+
+
+'''
+Output:
+Binary: 0b101
+Binary: 0b1100
+'''
+
