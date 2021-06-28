4e27f55ace17cec890b4b032643b0439669e58dc
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-28 08:58:40 +0530

    Bin Function Basic 2 source

---
+++ src/bin_function_basic2.py
@@ -0,0 +1,19 @@
+'''
+Python Bin Function (Basic 2)
+
+Example: Bin function examples 
+'''
+
+numbers = [11, -7, 16]
+
+# Display
+for num in numbers:
+    print(f"{num}: {bin(num)}")
+
+'''
+Output:
+11: 0b1011
+-7: -0b111
+16: 0b10000
+'''
+
