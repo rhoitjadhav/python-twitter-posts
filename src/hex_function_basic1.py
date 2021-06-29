e05d30151868d9efbc36ab73eedf4697b1ade10b
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-29 07:58:05 +0530

    Hex Function Basic 1 source

---
+++ src/hex_function_basic1.py
@@ -0,0 +1,19 @@
+'''
+Python Hex Function (Basic 1)
+
+Example: Hex function examples 
+'''
+num1 = 4
+num2 = 11
+
+# Display
+print("Hexadecimal:", hex(num1))
+print("Hexadecimal:", hex(num2))
+
+
+'''
+Output:
+Hexadecimal: 0x4
+Hexadecimal: 0xb
+'''
+
