50328dd66c0d5bdc066cd3ab11e756847fe38936
rhoitjadhav <jadhavrohit497@gmail.com>
2021-06-30 08:50:21 +0530

    Hex Function Basic 2 source

---
+++ src/hex_function_basic2.py
@@ -0,0 +1,18 @@
+'''
+Python Hex Function (Basic 2)
+
+Example: Hex function examples 
+'''
+numbers = [15, -9, 12]
+
+# Display
+for num in numbers:
+    print(f"{num}: {hex(num)}")
+
+'''
+Output:
+15: 0xf
+-9: -0x9
+12: 0xc
+'''
+
