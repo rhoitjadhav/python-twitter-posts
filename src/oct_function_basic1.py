3a41318c822679fb7563cbb53c6714d3ccc4eb7f
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-01 09:24:46 +0530

    Oct Function Basic 1 source

---
+++ src/oct_function_basic1.py
@@ -0,0 +1,20 @@
+'''
+Python Oct Function (Basic 1)
+
+Example: Oct function examples 
+'''
+num1 = 7
+num2 = 13
+
+
+# Display
+print("Octal:", oct(num1))
+print("Octal:", oct(num2))
+
+
+'''
+Output:
+Octal: 0o7
+Octal: 0o15
+'''
+
