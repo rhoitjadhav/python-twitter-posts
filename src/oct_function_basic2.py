6140e222595ba3e7bb2eef488c0a72e1048b0c59
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-02 08:34:15 +0530

    Oct Function Basic 2 source

---
+++ src/oct_function_basic2.py
@@ -0,0 +1,18 @@
+'''
+Python Oct Function (Basic 2)
+
+Example: Oct function examples 
+'''
+numbers = [22, -5, 11]
+
+# Display
+for num in numbers:
+    print(f"{num}: {oct(num)}")
+
+'''
+Output:
+22: 0o26
+-5: -0o5
+11: 0o13
+'''
+
