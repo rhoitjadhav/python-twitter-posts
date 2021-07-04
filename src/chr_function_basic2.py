e00492c31bf333f3ee143a0d8a8dfaaf9e1da0b7
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-04 09:00:42 +0530

    Chr Function Basic 2 source

---
+++ src/chr_function_basic2.py
@@ -0,0 +1,19 @@
+'''
+Python Chr Function (Basic 2)
+
+Example: Chr function examples
+'''
+
+unicodes = [100, 101, 102]
+
+# Display
+for unicode in unicodes:
+    print(f"{unicode}: {chr(unicode)}")
+
+'''
+Output:
+100: d
+101: e
+102: f
+'''
+
