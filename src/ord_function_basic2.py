429205502ef3258aaff39be1216fe8cb010ba5f4
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-06 08:20:51 +0530

    Ord Function Basic 2 source

---
+++ src/ord_function_basic2.py
@@ -0,0 +1,21 @@
+'''
+Python Ord Function (Basic 2)
+
+Example: Ord function examples
+'''
+characters = ["P", "q", "R", "s"]
+
+
+# Display
+for character in characters:
+    print(f"{character}: {ord(character)}")
+
+
+'''
+Output:
+P: 80
+q: 113
+R: 82
+s: 115
+'''
+
