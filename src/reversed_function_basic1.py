db6afed79326f9fb1c0c75a0f8a4bb407ffcf53e
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-07 08:47:29 +0530

    Reversed Function Basic 1 source

---
+++ src/reversed_function_basic1.py
@@ -0,0 +1,17 @@
+'''
+Python Reversed Function (Basic 1)
+
+Example: Reversed function examples
+'''
+seq = [1, 2, 3, 4]
+
+rev = reversed(seq)
+
+# Display
+print(list(rev))
+
+'''
+Output:
+[4, 3, 2, 1]
+'''
+
