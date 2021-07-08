e00c51e67dbdd5aaf8cb4f46e22caa2395bef1e6
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-08 16:34:46 +0530

    Reversed Function Basic 2 source

---
+++ src/reversed_function_basic2.py
@@ -0,0 +1,17 @@
+'''
+Python Reversed Function (Basic 2)
+
+Example: Reversed function examples
+'''
+seq = (4, 2, 2, 1)
+
+rev = reversed(seq)
+
+# Display
+print(tuple(rev))
+
+'''
+Output:
+(1, 2, 2, 4)
+'''
+
