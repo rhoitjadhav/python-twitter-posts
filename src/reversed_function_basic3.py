94883b3ef04e0d7a0d389e572c6b5f1fb858c3d3
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-09 10:03:47 +0530

    Reversed Function Basic 3 source

---
+++ src/reversed_function_basic3.py
@@ -0,0 +1,17 @@
+'''
+Python Reversed Function (Basic 3)
+
+Example: Reversed function examples
+'''
+string = "Hello World"
+
+rev = reversed(string)
+
+# Display
+print("".join(rev))
+
+'''
+Output:
+dlroW olleH
+'''
+
