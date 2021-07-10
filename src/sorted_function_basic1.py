212f4e533f37299caec2be7ddac4e6a1722f223a
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-10 08:11:47 +0530

    Sorted Function Basic 1 source

---
+++ src/sorted_function_basic1.py
@@ -0,0 +1,17 @@
+'''
+Python Sorted Function (Basic 1)
+
+Example: Sorted function examples
+'''
+numbers = (99, 1000, 767, 548, 666)
+
+nums = sorted(numbers)
+
+# Display
+print(nums)
+
+'''
+Output:
+[99, 548, 666, 767, 1000]
+'''
+
