5c15393c9fc5cad8f71cb2f912f3945a52238da9
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-11 09:18:50 +0530

    Sorted Function Basic 2 source

---
+++ src/sorted_function_basic2.py
@@ -0,0 +1,17 @@
+'''
+Python Sorted Function (Basic 2)
+
+Example: Sorted function examples
+'''
+string = "badc"
+
+sorted_string = sorted(string)
+
+# Display
+print("".join(sorted_string))
+
+'''
+Output:
+abcd
+'''
+
