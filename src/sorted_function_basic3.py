7ad2476583f7968250ee4a3cbe554e349fae5332
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-12 09:17:01 +0530

    Sorted Function Basic 3 source

---
+++ src/sorted_function_basic3.py
@@ -0,0 +1,17 @@
+'''
+Python Sorted Function (Basic 3)
+
+Example: Sorted function examples
+'''
+numbers = (99, 10, 123, 43)
+
+descending_sort = sorted(numbers, reverse=True)
+
+# Display
+print(descending_sort)
+
+'''
+Output:
+[123, 99, 43, 10]
+'''
+
