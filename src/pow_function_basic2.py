cc3b6ed120e6ee9f1ea26dece27291b5d6bc58c1
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-17 09:28:46 +0530

    Pow Function Basic 2 source

---
+++ src/pow_function_basic2.py
@@ -0,0 +1,18 @@
+'''
+Python Pow Function (Basic 2)
+
+Example: Pow Function Examples
+'''
+
+x = 11
+y = 2
+z = 5
+
+# Display
+print(pow(x, y, z))  # equivalent to x**y % z expression
+
+'''
+Output:
+1
+'''
+
