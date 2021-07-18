f603f2659e5993dc8d6612e6b51fbb8a06b1d654
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-18 09:19:25 +0530

    Pow Function Basic 3 source

---
+++ src/pow_function_basic3.py
@@ -0,0 +1,18 @@
+'''
+Python Pow Function (Basic 3)
+
+Example: Pow Function Examples
+'''
+
+# Display
+print(pow(2, -2))  
+print(pow(-4, 3))  
+print(pow(-5, -2))  
+
+'''
+Output:
+0.25
+-64
+0.04
+'''
+
