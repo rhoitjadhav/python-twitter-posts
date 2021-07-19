241642e3409ede99c5f76c3cf37c3dd4ded50fa0
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-19 09:05:32 +0530

    Divmod Function Basic 1 source

---
+++ src/divmod_function_basic1.py
@@ -0,0 +1,18 @@
+'''
+Python Divmod Function (Basic 1)
+
+Example: Divmod Function Examples
+'''
+
+quo, rem = divmod(5, 2)
+
+# Display
+print("Quotient:", quo)
+print("Remainder:", rem)
+
+'''
+Output:
+Quotient: 2
+Remainder: 1
+'''
+
