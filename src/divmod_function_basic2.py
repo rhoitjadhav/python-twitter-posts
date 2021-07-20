b0f7dd2d2580aaec1a456bb274b7ff0ce8815210
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-20 10:18:19 +0530

    Divmod Function Basic 2 source

---
+++ src/divmod_function_basic2.py
@@ -0,0 +1,18 @@
+'''
+Python Divmod Function (Basic 2)
+
+Example: Divmod Function Examples
+'''
+
+# Display
+print(divmod(5.0, 2.0))
+print(divmod(7.5, 2.5))
+print(divmod(10.5, 2.1))
+
+'''
+Output:
+(2.0, 1.0)
+(3.0, 0.0)
+(4.0, 2.0999999999999996)
+'''
+
