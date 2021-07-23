4dbfa84c69adf024933a206ddd6515b8e4ecaec4
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-23 12:13:37 +0530

    Bytes Function Basic 2 source

---
+++ src/bytes_function_basic2.py
@@ -0,0 +1,20 @@
+'''
+Python Bytes Function (Basic 2)
+
+Example: Bytes Function Examples
+'''
+
+size = 5
+
+# Bytes array 
+encoded = bytes(size)
+
+
+# Display
+print(f"Encoded: {encoded}")
+
+'''
+Output:
+Encoded: b'\x00\x00\x00\x00\x00'
+'''
+
