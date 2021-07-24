be435e3a0c65f4c8f1f6d1ce5303e4a725aed89f
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-24 10:19:42 +0530

    Bytes Function Basic 3 source

---
+++ src/bytes_function_basic3.py
@@ -0,0 +1,20 @@
+'''
+Python Bytes Function (Basic 3)
+
+Example: Bytes Function Examples
+'''
+
+iterable = [1, 2, 3]
+
+# Bytes array 
+encoded = bytes(iterable)
+
+
+# Display
+print(f"Encoded: {encoded}")
+
+'''
+Output:
+Encoded: b'\x01\x02\x03'
+'''
+
