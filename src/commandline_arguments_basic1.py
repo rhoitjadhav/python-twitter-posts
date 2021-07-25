7599e1ee13cdffd1338b70d194bd596cce3d005f
rhoitjadhav <jadhavrohit497@gmail.com>
2021-07-25 11:17:38 +0530

    Command Line Argument Basic 1 source

---
+++ src/commandline_arguments_basic1.py
@@ -0,0 +1,16 @@
+'''
+Python Commandline arguments (Basic 1)
+
+Example: Print commandline arguments 
+'''
+import sys
+
+args = sys.argv
+
+print("Args:", args)
+
+'''
+Output:
+Args: ['test.py', 'sdf']
+'''
+
