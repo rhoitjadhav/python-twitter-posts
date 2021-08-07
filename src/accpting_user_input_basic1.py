0df752312b4ffc1a76b39bcdfbfbe28469ed4ade
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-07 14:09:36 +0530

    Accepting User Input Basic 1 source

---
+++ src/accpting_user_input_basic1.py
@@ -0,0 +1,18 @@
+'''
+Python Accepting User Input
+
+Example: input() function examples
+'''
+
+name = input("Enter your name: ")
+
+
+print(f"Name: {name}")
+
+
+'''
+Output:
+Enter your name: Rohit
+Name: Rohit
+'''
+
