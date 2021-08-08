de655ae26f25161f749b73eca6ace937dcacdf39
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-08 14:43:44 +0530

    Accepting User Input Basic 2 source

---
+++ src/accpting_user_input_basic2.py
@@ -0,0 +1,18 @@
+'''
+Python Accepting User Input
+
+Example: input() function examples
+'''
+
+number = int(input("Enter number: "))
+
+
+print(f"Number: {number}")
+
+
+'''
+Output:
+Enter number: 12  
+Number: 12
+'''
+
