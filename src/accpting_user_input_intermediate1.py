884d3fb63216fd221b303b9bf1ca00d27bda6b27
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-09 22:52:14 +0530

    Accepting User Input Intermediate 1 source

---
+++ src/accpting_user_input_intermediate1.py
@@ -0,0 +1,18 @@
+'''
+Python Accepting User Input (Intermediate 1)
+
+Example: input() function examples
+'''
+
+numbers = list(map(int, input("Enter number: ").split()))
+
+
+print(f"Numbers List: {numbers}")
+
+
+'''
+Output:
+Enter number: 1 2 3 5
+Numbers List: [1, 2, 3, 5]
+'''
+
