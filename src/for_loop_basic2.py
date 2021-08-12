9ea35fae795523acb3fcaa8ed5339098d44c9a97
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-12 22:16:06 +0530

    For Loop Basic 2 source

---
+++ src/for_loop_basic2.py
@@ -0,0 +1,20 @@
+'''
+Python For Loop (Basic 2)
+
+Example: for loop examples
+'''
+numbers = [1, 2, 3, 4, 5]
+
+for i in range(len(numbers)):
+    print(numbers[i])
+
+
+'''
+Output:
+1
+2
+3
+4
+5
+'''
+
