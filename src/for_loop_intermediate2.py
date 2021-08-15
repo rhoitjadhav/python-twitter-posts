6a9cd27385fc8c82f7d79220f1a08a5c10df6dcc
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-15 17:05:43 +0530

    For Loop Intermediate 2 source

---
+++ src/for_loop_intermediate2.py
@@ -0,0 +1,25 @@
+'''
+Python For Loop (Intermediate 2)
+
+Example: for loop examples
+'''
+numbers = []
+
+print("Enter 3 Numbers:")
+
+for i in range(3):
+    num = int(input())
+    numbers.append(num)
+
+
+print("Numbers:", numbers)
+
+'''
+Output:
+Enter 3 Numbers:
+1
+2
+3
+Numbers: [1, 2, 3]
+'''
+
