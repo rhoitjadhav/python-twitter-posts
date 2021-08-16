466f4b0ae3e088f4f313c28a01d07fddf38e0ab1
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-16 18:04:50 +0530

    For Loop Intermediate 3 source

---
+++ src/for_loop_intermediate3.py
@@ -0,0 +1,21 @@
+'''
+Python For Loop (Intermediate 3)
+
+Example: for loop examples
+'''
+numbers = [9, 8, 7, 6, 5]
+
+
+for number in numbers:
+    print(number)
+
+
+'''
+Output:
+9
+8
+7
+6
+5
+'''
+
