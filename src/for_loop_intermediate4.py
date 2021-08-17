bdad0b98ec29e989583125499733a70c940f1361
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-17 20:42:21 +0530

    For Loop Intermediate 4 source

---
+++ src/for_loop_intermediate4.py
@@ -0,0 +1,21 @@
+'''
+Python For Loop (Intermediate 4)
+
+Example: for loop examples
+'''
+player = {
+    "Name": "Lebron James",
+    "Team": "LA Lakers"
+}
+
+
+for p in player:
+    print(f"{p}: {player[p]}")
+
+
+'''
+Output:
+Name: Lebron James
+Team: LA Lakers
+'''
+
