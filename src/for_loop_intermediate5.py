244a618735312abed297e9909b113dc8dd269686
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-18 17:32:04 +0530

    For Loop Intermediate 5 source

---
+++ src/for_loop_intermediate5.py
@@ -0,0 +1,21 @@
+'''
+Python For Loop (Intermediate 5)
+
+Example: for loop examples
+'''
+player = {
+    "Name": "Lebron James",
+    "Team": "LA Lakers"
+}
+
+
+for key, value in player.items():
+    print(f"{key}: {value}")
+
+
+'''
+Output:
+Name: Lebron James
+Team: LA Lakers
+'''
+
