e04270e2647d34d88e215762831f88bc5c7b7a67
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-19 16:20:04 +0530

    For Loop Advanced 1 source

---
+++ src/for_loop_advanced1.py
@@ -0,0 +1,18 @@
+'''
+Python For Loop (Advanced 1)
+
+Example: for loop break and continue examples
+'''
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
+
+'''
+Output:
+0
+1
+2
+3
+'''
+
