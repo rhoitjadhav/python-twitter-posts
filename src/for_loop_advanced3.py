16c0f8c78d29ca9144335f8c1e02607c8f26d9ad
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-21 19:33:26 +0530

    For Loop Advanced 3 source

---
+++ src/for_loop_advanced3.py
@@ -0,0 +1,22 @@
+'''
+Python For Loop (Advanced 3)
+
+Example: for loop and else clause
+'''
+n = 3
+
+for i in range(n):
+    print(i)
+
+else:
+    print("Else Clause")
+
+
+'''
+Output:
+0
+1
+2
+Else Clause
+'''
+
