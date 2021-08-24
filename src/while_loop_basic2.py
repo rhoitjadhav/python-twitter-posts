c902ffd51f61929d1bb0f38af7589edddce0d15e
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-24 18:44:23 +0530

    While Loop Basic 2 source

---
+++ src/while_loop_basic2.py
@@ -0,0 +1,22 @@
+'''
+Python While Loop (Basic 2)
+
+Example: while loop examples
+'''
+numbers = [1, 2, 3, 4, 5]
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
+
+
+'''
+Output:
+1
+2
+3
+4
+5
+'''
+
