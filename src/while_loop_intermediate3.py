506cc3be4b46ffb20d6d8a83e35a8704ea78eb0d
rhoitjadhav <jadhavrohit497@gmail.com>
2021-09-09 17:04:18 +0530

    While Loop Intermediate 3 source

---
+++ src/while_loop_intermediate3.py
@@ -0,0 +1,21 @@
+'''
+Python While Loop (Intermediate 3)
+
+Example: while loop examples
+'''
+
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+else:
+    print("Else Clause") 
+
+'''
+Output:
+0
+1
+2
+Else Clause
+'''
+
