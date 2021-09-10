f695fa3e2d5d513088bbec473631d83df7d47f71
rhoitjadhav <jadhavrohit497@gmail.com>
2021-09-10 12:29:39 +0530

    Exception Handling Basic 1 source

---
+++ src/exception_handling_basic1.py
@@ -0,0 +1,17 @@
+'''
+Python Exception Handling (Basic 1)
+
+Example: exception handling examples
+'''
+try:
+    pass
+
+except Exception as e:
+    print(repr(e))
+
+
+'''
+Output:
+<No-Output>
+'''
+
